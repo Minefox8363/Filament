3ffe1c0ab36e83f78313bf08eeaecae03c371261
Minefox8363 <09lucasjakub@gmail.com>
2025-12-01 00:29:18 +0000

    Add dark homepage theme and adjust login redirect

--- resources/views/components/sections/header.blade.php
+++ resources/views/components/sections/header.blade.php
@@ -1,4 +1,4 @@
-<header class="text-white bg-black shadow">
+<header class="text-white bg-gradient-to-r from-gray-950/80 via-gray-900/70 to-gray-950/80 backdrop-blur border-b border-white/10">
   <x-container>
     <nav class="flex items-center justify-between py-4">
       <a
@@ -14,7 +14,8 @@
         <x-button
           :icon="Auth::check() ? 'heroicon-o-cog' : 'heroicon-s-user'"
           size="xs"
-          :url="Filament\Pages\Dashboard::getUrl()"
+          :url="Filament\\Pages\\Dashboard::getUrl()"
+          class="!bg-white/10 !border-white/20 hover:!bg-white/20"
         >
           {{ Auth::check() ? 'Manage' : 'Login' }}
         </x-button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  darkMode: 'class',
   content: [
     './app/**/*.php',
     './config/**/*.php',
