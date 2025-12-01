ba0b64daf7d7ca20096150b68fcdfd1cf386bd9e
Minefox8363 <09lucasjakub@gmail.com>
2025-12-01 21:14:17 +0000

    Fix theme mode configuration

--- app/Providers/AdminPanelProvider.php
+++ app/Providers/AdminPanelProvider.php
@@ -14,7 +14,6 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Support\Enums\ThemeMode;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -68,7 +67,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
-            ->defaultThemeMode(ThemeMode::Dark)
+            ->defaultThemeMode('dark')
             ->viteTheme('resources/css/admin.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
